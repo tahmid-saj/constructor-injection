a975c596adc83b231e695e631adfec6b49c0d3fc
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-21 23:37:51 -0400

    primary injection

--- src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
@@ -1,5 +1,6 @@
 package com.luv2code.springcoredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -13,7 +13,7 @@ public class DemoController {
 
     // define a constructor for dependency injection
 //    @Autowired
-    public DemoController(@Qualifier("trackCoach") Coach theCoach) {
+    public DemoController(Coach theCoach) {
         myCoach = theCoach;
     }
 
