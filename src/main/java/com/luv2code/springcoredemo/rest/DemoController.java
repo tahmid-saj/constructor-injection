b66947df8b61a9f42234f77c46d65d6631863919
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-21 23:26:03 -0400

    Qualifiers

--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.luv2code.springcoredemo.rest;
 
 import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ public class DemoController {
 
     // define a constructor for dependency injection
 //    @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("trackCoach") Coach theCoach) {
         myCoach = theCoach;
     }
 
